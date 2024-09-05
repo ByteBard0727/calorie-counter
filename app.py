5ad8e7bcce1a08a9290058b48419c62fa2196a7e
bytebard.joey <146401220+ByteBard0727@users.noreply.github.com>
2024-09-05 23:07:12 +0900

    created db and linked containerized sql server to app

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 
-#This will be the home page
+#config SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:<YourPassword>@localhost/calorie-counter?driver=ODBC+Driver+17+for+SQL+Server'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#define the db
+db = SQLAlchemy
+
+#The homepage with, current date, date lastlogin and last weight/diet/
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    current_date = datetime.now() .strftime('%d-%m-%Y')
+    last_login = 
 
-@app.route('')
+    last_weight = 
+    last_diet =
+    return render_template('index.html', current_date=current_date')
 
 if __name__ == '__main__':
     app.run(debug=True)
