f912fe8b6dcad2ab959a086cbaeaf1862a129178
bytebard.joey <146401220+ByteBard0727@users.noreply.github.com>
2024-09-05 15:39:05 +0900

    Added read today's date feature for homepage

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-Flask import from flask
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+#This will be the home page
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('')
+
+if __name__ == '__main__':
+    app.run(debug=True)
