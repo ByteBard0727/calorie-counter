f395210ae38b64adbaa3a1b3c793596c915ae6bb
bytebard.joey <146401220+ByteBard0727@users.noreply.github.com>
2024-09-06 17:46:45 +0900

    added email, username, password references for the new db columns

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -16,6 +17,12 @@ class User(db.Model):
     __tablename__ = 'User'
     UserID = db.Column(db.Integer, primary_key=True)
     last_login = db.Column(db.DateTime)
+    weight = db.Column(db.Float)
+    calories = db.Column(db.Int)
+    Username = db.Column(db.String(50), unique=True, nullable=False)
+    Password = db.Column(db.String(128))
+    E-mail = db.Column(db.String(255), unique=True, nullable=False)
+
 
 #initialie the db
 with app.app_context():
@@ -29,7 +36,7 @@ def home():
     if user and user.last_login:
         last_login = user.last_login.strftime('%d-%m-%Y %H:%M:%S')
     else:
-        last_login = None
+        last_login = "Welcome!" Username "this is the first time you have logged in"
 
     current_date = datetime.now().strftime('%d-%m-%Y')
 
