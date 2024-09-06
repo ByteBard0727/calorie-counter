b0fab128a2a65af82913918edeccde74b41c104c
bytebard.joey <146401220+ByteBard0727@users.noreply.github.com>
2024-09-07 00:46:28 +0900

    Final edits app.py STILL NEED DEBUGGING

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from forms import RegistrationForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mew'  # For CSRF protection
 
 #config SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:<YourPassword>@localhost/calorie-counter?driver=ODBC+Driver+17+for+SQL+Server'
@@ -18,15 +20,37 @@ class User(db.Model):
     UserID = db.Column(db.Integer, primary_key=True)
     last_login = db.Column(db.DateTime)
     weight = db.Column(db.Float)
-    calories = db.Column(db.Int)
+    calories = db.Column(db.Integer)
     Username = db.Column(db.String(50), unique=True, nullable=False)
     Password = db.Column(db.String(128))
-    E-mail = db.Column(db.String(255), unique=True, nullable=False)
+    Email = db.Column(db.String(255), unique=True, nullable=False)
 
+    def set_Password(self, password):
+        self.Password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.Password, password)
 
 #initialie the db
 with app.app_context():
     db.create_all()
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # Check if email is already registered
+        if User.query.filter_by(email=form.Email.data).first():
+            flash('Email already registered. Please use a different one.')
+            return redirect(url_for('register'))
+
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.Email.data, password_hash=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successful! You can now log in.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 #The homepage with, current date, date lastlogin and last weight/diet/
 @app.route('/')
@@ -36,7 +60,7 @@ def home():
     if user and user.last_login:
         last_login = user.last_login.strftime('%d-%m-%Y %H:%M:%S')
     else:
-        last_login = "Welcome!" Username "this is the first time you have logged in"
+        last_login = "Welcome! this is the first time you have logged in"
 
     current_date = datetime.now().strftime('%d-%m-%Y')
 
--- forms.py
+++ forms.py
@@ -4,8 +4,8 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=100)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    Username = StringField('Username', validators=[DataRequired(), Length(min=4, max=100)])
+    Email = StringField('Email', validators=[DataRequired(), Email()])
+    Password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
