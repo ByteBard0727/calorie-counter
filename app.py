17fcf1e2bfd6607d57ebc94b9a01834aed5d3f72
bytebard.joey <146401220+ByteBard0727@users.noreply.github.com>
2024-09-10 11:37:20 +0900

    added Flask_login to protect different routes from being accessed without logging in

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import RegistrationForm, LoginForm
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
+
+# for CSRF protection
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mew'  # For CSRF protection
+app.config['SECRET_KEY'] = 'mew'
 
 #config SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:<YourPassword>@localhost/calorie-counter?driver=ODBC+Driver+17+for+SQL+Server'
@@ -14,8 +17,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #define the db
 db = SQLAlchemy(app)
 
+#init the loginmanager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
 #define the db model this will let flask-sqlalchemy map a route to the table and the right column created in dbeaver
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'User'
     UserID = db.Column(db.Integer, primary_key=True)
     last_login = db.Column(db.DateTime)
@@ -30,6 +38,10 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.Password, password)
+    
+#instructions for flasklogin to load users from dbeaver
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 #initialize the db
 with app.app_context():
@@ -39,7 +51,12 @@ with app.app_context():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('home')
+        user = User.query.filter_by(Username=form.Username.data).first()
+        if user and user.check_password(form.Password.data):
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Unable to login. Check your username and password.')
     return render_template('login.html', form=form)
 
 #Map to the register page using the forms from the imported Forms
@@ -47,7 +64,6 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        # Check if email is already registered
         if User.query.filter_by(Email=form.Email.data).first():
             flash('Email already registered. Please use a different one.')
             return redirect(url_for('register'))
@@ -62,11 +78,14 @@ def register():
 
 #The homepage with, current date, date lastlogin and last weight/diet/
 @app.route('/home')
-def home():
-    User = User.query.first()
 
-    if User and User.last_login:
-        last_login = User.last_login.strftime('%d-%m-%Y %H:%M:%S')
+@app.route('/dashboard')
+@login_required
+def home():
+    user = User.query.first()
+
+    if user and user.last_login:
+        last_login = user.last_login.strftime('%d-%m-%Y %H:%M:%S')
     else:
         last_login = "Welcome! this is the first time you have logged in"
 
@@ -75,4 +94,4 @@ def home():
     return render_template('index.html', current_date=current_date, last_login=last_login)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
