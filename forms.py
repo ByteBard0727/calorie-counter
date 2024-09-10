b6067032d8bd7296d1855b17505166dc1f793a4e
bytebard.joey <146401220+ByteBard0727@users.noreply.github.com>
2024-09-10 11:36:19 +0900

    adjusted forms.py so that the validation form passes

--- forms.py
+++ forms.py
@@ -9,3 +9,8 @@ class RegistrationForm(FlaskForm):
     Password = PasswordField('Password', validators=[DataRequired()])
     confirm_Password = PasswordField('confirm_Password', validators=[DataRequired(), EqualTo('Password')])
     submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    Username = StringField('Username', validators=[DataRequired(), Length(min=4, max=100)])
+    Password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
