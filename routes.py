31456fa329bcc77af3f442c40d2c029cd0bc98cd
bytebard.joey <146401220+ByteBard0727@users.noreply.github.com>
2024-09-18 19:17:37 +0900

    adjusted the handle from app.py to __init__.py after consolidating the code

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_user, login_required, logout_user, current_user
-from app import db
+from __init__ import db
 from forms import RegistrationForm, LoginForm
 from models import User, UserDiet
 from datetime import datetime, timedelta
