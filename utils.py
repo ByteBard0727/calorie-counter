0c17b10d34a12c637abc39f9e65e8271f5859511
bytebard.joey <146401220+ByteBard0727@users.noreply.github.com>
2024-09-18 19:20:50 +0900

    added def blocks get start and end of week/day/month functions thinking to refactor them into more general reusable blocks

--- utils.py
+++ utils.py
@@ -1,12 +1,23 @@
-from app import db
+from __init__ import db
 from models import UserDiet, UserExercise, UserWeight, UserGoal, User
 from datetime import datetime, timedelta
 
+#time functions start and end of (day,week,month,year)
 def get_start_and_end_of_day():
     today = datetime.now()
     start_of_day = datetime(today.year, today.month, today.day, 0, 0, 0)
     end_of_day = start_of_day + timedelta(days=1) - timedelta(seconds=1)
     return start_of_day, end_of_day
+
+def get_start_and_end_of_week(user_id):
+    today = datetime.now()
+    start_of_week = datetime(today.year, today.month, today.day, 0, 0, 0)
+    end_of_week = start_of_week + timedelta(days=7) - timedelta(seconds=1)
+
+def get_start_and_end_of_year(user_id):
+    today = datetime.now()
+    start_of_year = datetime(today.year, today.month, today.day, 0, 0, 0)
+    end_of_year = start_of_year + timedelta(days=365) - timedelta(seconds=1)
 
 def get_total_day_consumed_calories(user_id):
     start_of_day, end_of_day = get_start_and_end_of_day()
@@ -20,5 +31,9 @@ def get_total_day_consumed_calories(user_id):
         total_day_calories = 0
     return total_day_calories
 
+def get_start_and_end_of_week(user_id):
+    today = datetime.now()
+    start_of_week = datetime(today.year, today.month, today.day, 0, 0, 0)
+    end_of_week = start_of_week + timedelta(days=7) - timedelta(seconds=1)
+
 def get_total_week_consumed_calories(user_id):
-    start_of_week, end_of_week
