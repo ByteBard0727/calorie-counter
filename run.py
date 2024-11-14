6ec7a65f512c2ed7a81f35dae2ed514a5497380d
bytebard.joey <146401220+ByteBard0727@users.noreply.github.com>
2024-11-14 15:21:52 +0900

    changed import from app to __init__

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-from app import create_app
+from __init__ import create_app
 
 app = create_app()
 
-if __name__ == '__main':
+if __name__ == '__main__':
+    print("app created successfully")
     app.run(debug=True)
