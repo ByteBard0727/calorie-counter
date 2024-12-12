9ec94e518e9a4d823e234e89d5c1243667dad771
bytebard.joey <146401220+ByteBard0727@users.noreply.github.com>
2024-12-12 17:35:31 +0900

    imported config.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import DevelopmentConfig
 from datetime import datetime, timedelta
+from config import DevelopmentConfig, Config, ProductionConfig
 
 # Initialize extensions
 db = SQLAlchemy()
